c1b5f3603de20f9fd1aaef47486487e770630167
Tavarod <124653926+Tavarod@users.noreply.github.com>
2023-02-07 00:10:36 -0500

    Add files via upload

---
+++ ejercicio1_pais.py
@@ -0,0 +1,24 @@
+#asi lo hice yo
+pais = input("Pais de origen")
+continente = input("Continente de residencia")
+print(type(f"{pais}{continente}"))
+print(f"Tu vives en :{pais} pero ahora vives en, {continente}☺")
+#asi lo hace el profesor
+paisB = "colombia"
+continenteB = "europa"
+año = int(input("Ingrese el año actual"))
+print(f"{paisB} - {continenteB}  - {str(año)}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
