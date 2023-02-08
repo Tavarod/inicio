b1e28d5d459d4dd3c72663013eba870e23eba723
Tavarod <124653926+Tavarod@users.noreply.github.com>
2023-02-07 19:01:35 -0500

    Add files via upload

---
+++ ejercicio9_passwords.py
@@ -0,0 +1,9 @@
+"""solicitar pass al usuario"""
+contador = 0
+while contador <= 1000:
+    contraseña = input("ingrese la contraseña")
+    if contraseña == "pochoposaurio1995":
+        print("por favor dirigase a la puerta numero 11")
+        break
+    else:
+        print("contraseña invalida Ammit va a hablar con tigo ☺")
